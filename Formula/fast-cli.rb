293432e99565b72cc3688b43edf77889bccb7782
Anton Melnikov <antonsm21@gmail.com>
2026-06-21 13:45:42 +0000

    fast-cli: add 0.3.5 bottle.

--- Formula/fast-cli.rb
+++ Formula/fast-cli.rb
@@ -6,6 +6,12 @@ class FastCli < Formula
   license "MIT"
   head "https://github.com/mikkelam/fast-cli.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/botantony/homebrew-fast-cli/releases/download/fast-cli-0.3.5"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "a45c5aa1a8ef0a5e830df15e2bbbe69c2ffe9a428c8c41c55c5d88d7f09e5af1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "8e07409f57d98cf964780e42ab3a537cdd117e627db95ce6010d01a6ef56140c"
+  end
+
   depends_on "zig@0.15" => :build
 
   def install
